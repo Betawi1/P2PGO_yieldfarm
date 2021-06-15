362cec9552085f4827bd5482770eb5125b6ae54b
Betawi1 <Rashdan.ibrahim@gmail.com>
2021-06-15 20:31:38 +0800

    Add files via upload
    Button for Uniswap

---
+++ src/components/TopBar/components/UniButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import useModal from '../../../hooks/useModal'
+import Button from '../../Button'
+import UniModal from './UniModal'
+
+interface UniButtonProps {}
+
+const UniButton: React.FC<UniButtonProps> = (props) => {
+  const [onPresentAccountModal] = useModal(<UniModal />)
+
+
+  return (
+    <StyledAccountButton>
+      {(
+        <Button onClick={onPresentAccountModal} size="sm" text="Uniswap" />
+      )}
+    </StyledAccountButton>
+  )
+}
+
+const StyledAccountButton = styled.div``
+
+
+export default UniButton
+
+// <StyledAccountButton>
+//    {(
+//      <Button onClick={onPresentAccountModal} size="sm" text="Uniswap" />
+//    )}
+//  </StyledAccountButton>
+//)
+//}
+
+//const StyledAccountButton = styled.div``
